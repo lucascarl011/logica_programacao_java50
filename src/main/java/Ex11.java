20f29aca3a6999ee795d238c9b7d1184329102c0
lucascarl011 <lucassscarlosss54@gmail.com>
2025-07-06 15:57:45 -0300

    Resolution Ex11

--- src/main/java/Ex11.java
+++ src/main/java/Ex11.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 /*
-*
+* Ler a nota 1 e 2 de um aluno, calcule a média e diga se ele foi aprovado, ou reprovado.
+* Considere Aprovado acima de 7.0.
 * */
 
 public class Ex11 {
@@ -9,5 +10,19 @@ public class Ex11 {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite sua primeira nota: ");
+        double n1 = sc.nextDouble();
+        System.out.println("Digite sua segunda nota: ");
+        double n2 = sc.nextDouble();
+
+        double media = (n1 + n2) / 2;
+
+        if (media >= 7.0) {
+            System.out.println("Sua média foi: " + media + " - Aluno Aprovado!");
+        }else {
+            System.out.println("Sua média foi: " + media + " - Aluno Reprovado!");
+        }
+
+        sc.close();
     }
 }
