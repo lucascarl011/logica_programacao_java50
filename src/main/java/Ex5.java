633ada04f1ed0fb2d7c18fbd2f29a0c649b42dab
lucascarl011 <lucassscarlosss54@gmail.com>
2025-07-06 14:01:24 -0300

    Resolution Ex5

--- src/main/java/Ex5.java
+++ src/main/java/Ex5.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
+/*
+* Escreva um algoritmo para ler o salario mensal atual de um funcionario e o percentual de reajuste.
+* Calcular e escrever o valor do novo salario.
+* */
+
 public class Ex5 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu sálario atual: ");
+        double salarioAtual = sc.nextDouble();
+        System.out.println("Digite o percentual de reajuste: ");
+        double percentualReajuste = sc.nextDouble();
+
+        double aumento = percentualReajuste * salarioAtual / 100;
+        double novoSalario = salarioAtual + aumento;
+
+        System.out.println("Novo sálario com aumento de 15% R$: " + novoSalario);
+
+        sc.close();
     }
 }
